22b966d6886dc56f629e8b2c00d9adfd95b6805d
cursosbackend <cursosbackends@gmail.com>
2025-07-30 02:26:35 +0000

    test

---
+++ app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import Producto
+# Create your views here.
+
+def home(request):
+    productos = Producto.objects.all()
+    return render(request, "app/home.html", {"productos": productos})
+
+def about(request):
+    return render(request, "app/about.html", {})
